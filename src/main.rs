96b6bc6d2923a8c9245ae7b1eed20ac0e715cb77
Sparsh <sparshag2803@gmail.com>
2026-01-31 19:29:09 +0530

    made a freestanding rust binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std] // unlink the Rust standard library
+#![no_main] // disable main entry points
+
+use core::panic::PanicInfo;
+
+#[unsafe(no_mangle)] 
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
+    loop {}
 }
+
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
