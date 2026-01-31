73817b00e2d8088f8dbcc02ff3986972f01f656b
Sparsh <sparshag2803@gmail.com>
2026-01-31 20:08:59 +0530

    made a rust kernel that prints hello world

--- rust_course/target/debug/deps/rust_course.d
+++
@@ -1,5 +0,0 @@
-C:\Users\kabir\Desktop\OSdev\try_os\rust_course\target\debug\deps\rust_course.d: src\main.rs
-
-C:\Users\kabir\Desktop\OSdev\try_os\rust_course\target\debug\deps\rust_course.exe: src\main.rs
-
-src\main.rs:
--- rust_course/target/debug/rust_course.d
+++
@@ -1,1 +0,0 @@
-C:\Users\kabir\Desktop\OSdev\try_os\rust_course\target\debug\rust_course.exe: C:\Users\kabir\Desktop\OSdev\try_os\rust_course\src\main.rs
